db5754f58d94a90ce14c9587cbc06321ab9e499f
judos <judos@gmx.ch>
2017-04-02 15:33:53 +0200

    v. 3.1

--- src/ch/judos/backupManager/Launcher.java
+++ src/ch/judos/backupManager/Launcher.java
@@ -8,7 +8,7 @@ import ch.judos.backupManager.view.MainFrame;
 import ch.judos.generic.exception.GlobalExceptionHandler;
 
 public class Launcher {
-	public static final String VERSION = "3.0";
+	public static final String VERSION = "3.1";
 	private PathStorage storage;
 	private static final File BACKUP_PATH_FILE = new File(Constants.APP_DATA,
 		"backupPaths.csv");
